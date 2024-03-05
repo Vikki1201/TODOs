d2c4a258d5f83c49a6b58ff1f132cf9d88934183
Viktoriia <viktoriia.koyun@yandex.com>
2024-03-05 05:06:51 +0300

    updated

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
                 if ((filter === 'all') ||
                     (filter === 'active' && !task.completed) ||
                     (filter === 'completed' && task.completed)) {
-                        var textDecoration = task.completed ? 'line-through' : 'none';
+                    var textDecoration = task.completed ? 'line-through' : 'none';
                     $('#todo-list').append('<div class="row"><div class="col-xs-8 d-flex justify-content-start"><input type="checkbox" class="mark-complete me-3" data-id="' + task.id + '"' + (task.completed ? 'checked' : '') + '><span class="text-center p-2" style="text-decoration:' + textDecoration + '";>' + task.content + '</span><button type="button" class="btn delete ms-auto" data-id="' + task.id + '"><svg width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16"><path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/></svg></button></div><hr/></div>');
                 }
             });
@@ -71,7 +71,7 @@ $(document).ready(function() {
             url: 'https://fewd-todolist-api.onrender.com/tasks/' + id + '/mark_complete?api_key=1209',
             dataType: 'json',
             success: function (response, textStatus) {
-                getAndDisplayAllTasks(filter);
+                getAndDisplayAllTasks('all');
             },
             error: function (request, textStatus, errorMessage) {
                 console.log(errorMessage);
@@ -85,7 +85,7 @@ $(document).ready(function() {
                 url: 'https://fewd-todolist-api.onrender.com/tasks/' + id + '/mark_active?api_key=1209',
                 dataType: 'json',
                 success: function (response, textStatus) {
-                    getAndDisplayAllTasks(filter);
+                    getAndDisplayAllTasks('all');
                 },
                 error: function (request, textStatus, errorMessage) {
                     console.log(errorMessage);
